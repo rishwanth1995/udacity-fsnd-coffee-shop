21480552776344a6620a4c920ed5734575334f61
Rishwanth Senthilkumar <rishwanthsenthilkumar@Rishwanths-MacBook-Pro.local>
2020-06-11 12:23:01 -0400

    change  jwt token in postman collection

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "udacity-fsnd-coffee.auth0.com", // the auth0 domain prefix
+    url: "udacity-fsnd-coffee", // the auth0 domain prefix
     audience: "coffee-shop", // the audience set for the auth0 app
     clientId: "HO2wQDUBlbUrIilLABoBxnKXnfL4nZ7b", // the client id generated for the auth0 app
     callbackURL: "http://localhost:8100", // the base url of the running ionic application.
